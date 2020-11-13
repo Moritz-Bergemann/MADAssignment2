50ec266a96121d95416d81fea056c26cdd27366c
Moritz Bergemann <moritz.bergemann@gmail.com>
2020-11-13 14:28:49 +0800

    Removed city info fragment and moved relevant information into header bar

--- app/src/main/java/com/moritzbergemann/myapplication/HeaderBarFragment.java
+++ app/src/main/java/com/moritzbergemann/myapplication/HeaderBarFragment.java
@@ -1,11 +1,15 @@
 package com.moritzbergemann.myapplication;
 
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +19,7 @@ import android.widget.TextView;
 import com.moritzbergemann.myapplication.model.GameData;
 import com.moritzbergemann.myapplication.model.UIUpdateObserver;
 
+import java.net.URL;
 import java.util.Locale;
 
 /**
@@ -28,6 +33,10 @@ public class HeaderBarFragment extends Fragment implements UIUpdateObserver {
     private TextView mPopulationValue;
     private TextView mMoneyPerTurnValue;
     private TextView mTimeValue;
+    private TextView mEmploymentRateValue;
+    private TextView mTemperatureValue;
+
+    private static final int WEATHER_UPDATE_DELAY = 1000 * 10;
 
     public HeaderBarFragment() {
         // Required empty public constructor
@@ -74,15 +83,12 @@ public class HeaderBarFragment extends Fragment implements UIUpdateObserver {
         mTimeValue = view.findViewById(R.id.timeValue);
         mTimeValue.setText(String.format(Locale.US, "%d", GameData.get().getGameTime()));
 
+        mEmploymentRateValue = view.findViewById(R.id.employmentRateValue);
+        mEmploymentRateValue.setText(String.format(Locale.US, "%.2f%%", GameData.get().getEmploymentRate() * 100.0));
+
         Button timeStepButton = view.findViewById(R.id.timeStepButton);
         timeStepButton.setOnClickListener(clickedTimeStepButton -> {
             GameData.get().timeStep();
-        });
-
-        Button cityInfoButton = view.findViewById(R.id.cityInfoButton);
-        cityInfoButton.setOnClickListener(view1 -> {
-            CityInfoFragment cityInfoFragment = CityInfoFragment.newInstance();
-            cityInfoFragment.show(getParentFragmentManager().beginTransaction(), "cityInfoDialog");
         });
 
         //Show the game over message if the game has been lost
@@ -95,6 +101,26 @@ public class HeaderBarFragment extends Fragment implements UIUpdateObserver {
                 gameLostMessage.setVisibility(View.VISIBLE);
             }
         });
+
+        //***GET CURRENT TEMPERATURE***
+        mTemperatureValue = view.findViewById(R.id.temperatureValue);
+
+        //Perform weather info refresh periodically
+        Handler temperatureUpdateHandler = new Handler();
+        Runnable runnableCode = new Runnable() {
+            @Override
+            public void run() {
+                URL requestUrl = Weather.makeWeatherAPIRequestUrl(GameData.get().getSettings().getCityName());
+                new GetTemperaturesTask().execute(requestUrl);
+
+                //Repeat the action after interval
+                temperatureUpdateHandler.postDelayed(this, WEATHER_UPDATE_DELAY);
+            }
+        };
+
+        //Initiate the first refresh
+        temperatureUpdateHandler.post(runnableCode);
+
 
         //Start listening for UI updates
         GameData.get().addUIUpdateObserver(this);
@@ -109,5 +135,56 @@ public class HeaderBarFragment extends Fragment implements UIUpdateObserver {
         mPopulationValue.setText(String.format(Locale.US, "%d", GameData.get().getPopulation()));
         mMoneyPerTurnValue.setText(String.format(Locale.US, "%d", GameData.get().getLatestIncome()));
         mTimeValue.setText(String.format(Locale.US, "%d", GameData.get().getGameTime()));
+        mEmploymentRateValue.setText(String.format(Locale.US, "%.2f%%", GameData.get().getEmploymentRate() * 100.0));
     }
+
+    /**
+     * Asynchronous task for displaying temperature data. This implementation only affects the value
+     *  of 'mTemperatureValue'. Sets the text to 'loading' when not done and displays temperature or
+     *  error once done
+     */
+    private class GetTemperaturesTask extends AsyncTask<URL, String, String> {
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        protected String doInBackground(URL... params) {
+            String temperatureString = "NULL";
+
+            if (params.length == 0) {
+                throw new IllegalArgumentException("At least one url param required");
+            }
+
+            publishProgress("Loading...");
+
+            //DO DOWNLOAD & WHATNOT HERE
+            for (URL url : params) {
+                try {
+                    double temperature = Weather.getTemperature(url);
+                    temperatureString = String.format(Locale.US, "%.2f°C", temperature);
+                } catch (WeatherException w) {
+                    temperatureString = "Failed: " + w.getMessage();
+                }
+            }
+
+            publishProgress("Done!");
+
+            return temperatureString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            mTemperatureValue.setText(result);
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+
+            for (String value : values) {
+                mTemperatureValue.setText(value);
+            }
+        }
+    }
+
 }
